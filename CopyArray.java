9b2538058ce935b4f018d93946bd7bf712e8c5bf
LiShixi <loyy77@gmail.com>
2012-06-26 18:37:19 +0800

    System.arraycopy(1,2,3,4,5)方法的使用 从指定源数组中复制一个数组，复制从指定的位置开始，到目标数组的指定位置结束。

---
+++ CopyArray.java
@@ -0,0 +1,35 @@
+/**
+ * 使用 System.arraycopy(src, srcPos, dest, destPos, length);复制数组，据说比循环快！！ 参数： src
+ * - 源数组。 srcPos - 源数组中的起始位置。 dest - 目标数组。 destPos - 目标数据中的起始位置。 length -
+ * 要复制的数组元素的数量。
+ * 
+ * @author LiShixi
+ * 
+ */
+public class CopyArray {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		int[] arr1 = new int[10000];
+		int[] arr2 = new int[5000];
+		for (int i = 0; i < 10000; i++) {
+			arr1[i] = i;
+			// 使用StringBuilder拼接字符串提高性能
+			StringBuilder sb = new StringBuilder(30000);
+			System.out.print(sb.append(arr1[i]).append(","));
+		}
+		// 执行复制操作
+		System.arraycopy(arr1, 0, arr2, 0, 5000);
+		System.out.println();
+		for (int i = 0; i < 5000; i++) {
+
+			StringBuilder sb = new StringBuilder(20000);
+			System.out.print(sb.append(arr2[i]).append(","));
+		}
+
+	}
+
+}
