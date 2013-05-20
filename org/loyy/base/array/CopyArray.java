95f155af5dcee2a0edff097ea76fd84410027763
LiShixi <loyy77@gmail.com>
2013-05-20 11:50:18 +0800

    修复乱码的注释，之前乱码的注释删除，这次是追加注释

--- org/loyy/base/array/CopyArray.java
+++ org/loyy/base/array/CopyArray.java
@@ -1,9 +1,13 @@
 package org.loyy.base.array;
 
 /**
+ * 使用 System.arraycopy(src, srcPos, dest, destPos, length);复制数组，据说比循环快！！ 参数： src
+ * - 源数组。 srcPos - 源数组中的起始位置。 dest - 目标数组。 destPos - 目标数据中的起始位置。 length -
+ * 要复制的数组元素的数量。
+ *
  * @author LiShixi
  */
-public class CopyArray {
+class CopyArray {
 
     /**
      * @param args
@@ -14,11 +18,11 @@ public class CopyArray {
         int[] arr2 = new int[5000];
         for (int i = 0; i < 10000; i++) {
             arr1[i] = i;
-
+            // 使用StringBuilder拼接字符串提高性能
             StringBuilder sb = new StringBuilder(30000);
             System.out.print(sb.append(arr1[i]).append(","));
         }
-
+        // 执行复制操作
         System.arraycopy(arr1, 0, arr2, 0, 5000);
         System.out.println();
         for (int i = 0; i < 5000; i++) {
